02e7ba90a7a8a259266ba228e50abecc63c84a15
Gaurav Singh <gaurav.singh@cdot.in>
2024-06-21 17:09:12 +0530

    Added the coginito settings

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "us-west-2_El3TLQDef", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "3udpat8h9u8edoefdnq1spihcv", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-west-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "https://wildrydeswebapp.auth.us-west-2.amazoncognito.com", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
